40c9550f8b8aa3e8c92e32b6c359ae7f202f0900
zenix-s <sergiofs19@gmail.com>
2024-05-15 22:35:38 +0200

    up
    up

--- .config/nvim/lua/zenix/plugins/42header.lua
+++ .config/nvim/lua/zenix/plugins/42header.lua
@@ -1,15 +1,11 @@
 return {
   "Diogo-ss/42-header.nvim",
-  cmd = { "Stdheader" },
-  keys = {"<F1>"},
-  opts = {
-    default_map = true, -- Default mapping <F1> in normal mode.
-    auto_update = true, -- Update header when saving.
-    user = "serferna", -- Your user.
-    mail = "serferna@student.42madrid.com", -- Your mail.
-    -- add other options.
-  },
-  config = function(_, opts)
-    require("42header").setup(opts)
+  config = function()
+    require("42header").setup({
+      default_map = true,
+      auto_update = true,
+      user = "serferna",
+      mail = "serferna@student.42madrid.com",
+    })
   end,
 }
--- .config/nvim/lua/zenix/plugins/treesitter.lua
+++ .config/nvim/lua/zenix/plugins/treesitter.lua
@@ -8,6 +8,7 @@ return {
     local treesitter = require("nvim-treesitter.configs")
 
     treesitter.setup({
+      auto_install = true,
       highlight = {
         enable = true,
       },
